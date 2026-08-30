6be17d39f752e5492124bac43f4f147c6036bd80
EloiGP10 <eloi@eloiserv.com>
2026-08-30 15:48:55 +0200

    feat: profesorapp completo - Cuaderno docente + ITACA + 28 modelos Prisma

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
+  images: {
+    unoptimized: true,
+  },
   experimental: {
     serverActions: {
       bodySizeLimit: "10mb",
--- src/generated/client/edge.js
+++ src/generated/client/edge.js
@@ -729,6 +729,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
